d07340f409d3ab45191774cb52441abbf1f0e474
eughrat <michal.piernicki@gmail.com>
2022-02-11 17:49:59 +0100

    added html fixed few bugs

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('post/(<pk>\d+/remove', views.PostDeleteView.as_view(),name='post_delete'),
     path('drafts/', views.DraftListView.as_view(),name='post_draft_list'),
     path('post/<pk>\d+/comment/', views.add_comment_to_post,name='add_comment_to_post'),
-    path('comment/<pk>\d+/approve/', views.comment_appove,name='comment_approve'),
+    path('comment/<pk>\d+/approve/', views.comment_approve,name='comment_approve'),
     path('comment/<pk>\d+/remove/', views.comment_remove,name='comment_remove'),
     path('post/<pk>\d+/publish/', views.post_publish,name='post_publish'),
     ]
